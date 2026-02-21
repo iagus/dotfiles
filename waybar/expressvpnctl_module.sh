be8a93e931737534b90a989dd437d9a4e6c337f6
iagus <santiago.fgonz@gmail.com>
2026-02-21 16:58:40 +0800

    Waybar styles cleanup

--- waybar/expressvpnctl_module.sh
+++ waybar/expressvpnctl_module.sh
@@ -2,6 +2,6 @@
 
 connstate=$(expressvpnctl get connectionstate)
 region=$(expressvpnctl get region)
-output=$(printf '{"text": "%s", "tooltip": "%s at %s" }' "$connstate" "$connstate" "$region")
+output=$(printf '{"tooltip": "%s at %s" }' "$connstate" "$region")
 
 echo "$output" | jq --unbuffered --compact-output .
